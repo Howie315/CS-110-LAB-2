bdc5e778ea0197d4a3d669ce1fef6f3314751514
Howie Nguyen <nguyen.howie2010@gmail.com>
2023-04-17 15:42:02 -0700

    Added the constructor

---
+++ CS 110 Lab 2/main.js
@@ -0,0 +1,18 @@
+class TicTacToe {
+	constructor() {
+		this.gameBoard = {
+			one: "",
+			two: "",
+			three: "",
+			four: "",
+			five: "",
+			six: "",
+			seven: "",
+			eight: "",
+			nine: "",
+		};
+
+		this.currentPlayer = "X";
+		this.numMoves = 0;
+	}
+}
